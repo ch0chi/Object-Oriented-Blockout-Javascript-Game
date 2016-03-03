bf740467017a02a67e1f2631ca7d1f3f5b1476ff
Michael Quattrochi <mquattrochi@gmail.com>
2016-03-03 06:34:57 -0600

    Added extra paddle methods and paddle collision

--- functions.js
+++ functions.js
@@ -1,13 +1,19 @@
-//initialize variables
-
-
+//initialize Ball variables
 var canvas = document.getElementById("myCanvas");
 var ctx=canvas.getContext("2d");
 var x=canvas.width/2;//x ball position
-var y = canvas.height-30;//y ball posi
-var dx=2;
-var dy=-2;
+var y = canvas.height-30;//y ball position
+var dx=2;//shift on x-axis
+var dy=-2;//shift on y-axis
 var ballRadius = 10;
+var rightPressed = false;
+var leftPressed=false;
+
+
+
+/**
+*Ball class
+*/
 function Ball(){
 
 	
@@ -21,62 +27,117 @@ Ball.prototype.drawBall=function(){
 	ctx.fill();
 	ctx.closePath();
 }
+Ball.prototype.drawPaddle=function(){
+
+}
 //dynamically draw balls position
 Ball.prototype.draw=function(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	this.drawBall();
+	paddle.drawPaddle();
+	paddle.movePaddle();
 	x+=dx;
 	y+=dy;
 	this.x=x;
 	this.y=y;
 	this.checkWalls(this.x,this.y);
 }
+
+//refresh frame every 10milliseconds to see location of ball
 Ball.prototype.refresh=function(){
 	setInterval(this.draw.bind(this),8);
 }
-//refresh frame every 10milliseconds to see location of ball
-//setInterval(draw,8);
+
+
 
 Ball.prototype.checkWalls=function(x,y){
 	//reverse direction if ball hits top or bottom
-	console.log(dy);
-	if(y+dy> canvas.height-ballRadius || y + dy <ballRadius){
-		dy=-dy;
-		console.log(dy);
+	
+	if(x+dx>canvas.width-ballRadius || x+dx<ballRadius){
+		dx=-dx;
+	}
+
+	if(y + dy <ballRadius){
+		dy=-dy;		
+	}else if(y+dy>canvas.height-ballRadius){
+		if(x>paddleX && x <paddleX + paddleWidth){
+			dy=-dy;
+		}else{
+			//alert("Game Over");
+			$('#myModal').modal("show");
+			$('#closeModal').onclick=function(){
+
+				document.location.reload();
+				$('#myModal').modal('hide');
+			}
+			
+		}
+	}
+		
+
+
 		
 	}
 	//reverse direction if ball hits left or right
-	if(x+dx>canvas.width-ballRadius || x+dx<ballRadius){
-		dx=-dx;
-	}	
+	
+
+
+
+
+
+//initialize Paddle variables
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+/**
+*Paddle class
+*/
+function Paddle(){
+
 }
 
+Paddle.prototype.drawPaddle=function(){
+	ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-
-
-
-//intialize Paddle Variables
-var paddleHeight = 10;
-var paddleWidth=75;
-var paddleX=(canvas.width-paddleWidth)/2;
-
-//Paddle constructor
-function Paddle(){
-	Paddle.prototype.drawPaddle(){
-		ctx.beginPath();
-		ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
-		ctx.fillStyle="#0095DD";
-		ctx.fill();
-		ctx.closePath();
+Paddle.prototype.keyDownHandler=function(e){
+	if(e.keyCode==39){
+		rightPressed=true;
+	}else if(e.keyCode==37){
+		leftPressed=true;
 	}
 }
 
-//instantiate objects
-var ball = new Ball();
-
-//call methods
-function initialize(){	
-	ball.refresh();
+Paddle.prototype.keyUpHandler=function(e){
+	if(e.keyCode==39){
+		rightPressed=false;
+	}else if(e.keyCode==37){
+		leftPressed=false;
+	}
 }
 
-//define paddle
+Paddle.prototype.movePaddle=function(){
+	if(rightPressed && paddleX < canvas.width-paddleWidth){
+		paddleX+=7;
+	}else if(leftPressed && paddleX>0){
+		paddleX-=7;
+	}
+}
+
+
+//instantiate functions (classes)
+var ball = new Ball();
+var paddle = new Paddle();//called in draw method
+
+//call methods
+function initialize(){
+	document.addEventListener("keydown", paddle.keyDownHandler, false);
+	document.addEventListener("keyup", paddle.keyUpHandler, false);	
+	ball.refresh();
+}
+initialize();

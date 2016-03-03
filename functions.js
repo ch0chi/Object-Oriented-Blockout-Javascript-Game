83d0fd743aff3557209c8a72ba55ecf9df05b8d9
Michael Quattrochi <mquattrochi@gmail.com>
2016-03-03 17:46:42 -0600

    Added paddle movement and block collision

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+/**
+*Ball class
+*/
 //initialize Ball variables
 var canvas = document.getElementById("myCanvas");
 var ctx=canvas.getContext("2d");
@@ -9,11 +12,6 @@ var ballRadius = 10;
 var rightPressed = false;
 var leftPressed=false;
 
-
-
-/**
-*Ball class
-*/
 function Ball(){
 
 	
@@ -33,9 +31,18 @@ Ball.prototype.drawPaddle=function(){
 //dynamically draw balls position
 Ball.prototype.draw=function(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	paddle.drawPaddle();//must be called in draw
+	paddle.movePaddle();//must be called in draw	
 	this.drawBall();
-	paddle.drawPaddle();
-	paddle.movePaddle();
+
+	
+
+	
+	
+	
+	
+	bricks.collisionDetection();
 	x+=dx;
 	y+=dy;
 	this.x=x;
@@ -45,54 +52,53 @@ Ball.prototype.draw=function(){
 
 //refresh frame every 10milliseconds to see location of ball
 Ball.prototype.refresh=function(){
-	setInterval(this.draw.bind(this),8);
+	
+	setInterval(this.draw.bind(this),15);
+
 }
 
 
 
 Ball.prototype.checkWalls=function(x,y){
-	//reverse direction if ball hits top or bottom
-	
+	//reverse direction if ball hits left or right
 	if(x+dx>canvas.width-ballRadius || x+dx<ballRadius){
 		dx=-dx;
 	}
 
+	//if ball hits top then reverse, if ball hits bottom then show modal message
 	if(y + dy <ballRadius){
-		dy=-dy;		
+		dy=-dy;
+
+	//if ball hits paddle, then reverse			
 	}else if(y+dy>canvas.height-ballRadius){
+
 		if(x>paddleX && x <paddleX + paddleWidth){
 			dy=-dy;
 		}else{
-			//alert("Game Over");
 			$('#myModal').modal("show");
 			$('#closeModal').onclick=function(){
 
 				document.location.reload();
 				$('#myModal').modal('hide');
-			}
-			
+			}			
 		}
 	}
-		
-
-
-		
-	}
-	//reverse direction if ball hits left or right
+	
+}
+	
 	
 
 
 
-
+/**
+*Paddle class
+*/
 
 //initialize Paddle variables
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 
-/**
-*Paddle class
-*/
 function Paddle(){
 
 }
@@ -119,6 +125,7 @@ Paddle.prototype.keyUpHandler=function(e){
 	}else if(e.keyCode==37){
 		leftPressed=false;
 	}
+	
 }
 
 Paddle.prototype.movePaddle=function(){
@@ -127,17 +134,103 @@ Paddle.prototype.movePaddle=function(){
 	}else if(leftPressed && paddleX>0){
 		paddleX-=7;
 	}
+	x=x+dx;
+	y=y+dy;
 }
 
+
+/**
+*Brick class
+*/
+
+//Brink variables
+var brickRowCount=3;
+var brickColumnCount=5;
+var brickWidth=75;
+var brickHeight=20;
+var brickPadding = 10;
+var brickOffsetTop=30;
+var brickOffsetLeft=30;
+var bricks = [];
+
+function Bricks(){
+	
+}
+
+//generates bricks in a two-dimensional array
+Bricks.prototype.generateBricks=function(){
+	for(col=0;col<brickColumnCount;col++){
+		bricks[col]=[];
+		for(row=0;row<brickRowCount;row++){
+			bricks[col][row]={x:0,y:0,status:1};
+
+		}
+	}
+}
+
+//draws bricks to canvas
+Bricks.prototype.drawBricks=function(){	
+	//this.collisionDetection();
+	for(col=0;col<brickColumnCount;col++){
+		for(row=0;row<brickRowCount;row++){
+			//checks if bricks status is 1. If 1, draw brick. If 0, remove brick.
+			if(bricks[col][row].status==1){
+				var brickX = (col*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
+				bricks[col][row].x=brickX;
+				bricks[col][row].y=brickY;
+				ctx.beginPath();
+				ctx.rect(brickX,brickY,brickWidth,brickHeight);
+				ctx.fillStyle="#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}			
+
+		}
+	}
+}
+
+/**
+*Check if ball hits a brick
+*
+*x-position of the ball is > x-position of brick
+*x-position of the ball is <x-position of brick+width
+*y-position of the ball is > y-position of the brick
+*y-position of ball is < y-positoin of the brick plus its height
+*/
+Bricks.prototype.collisionDetection=function(){
+	bricks.drawBricks();
+	var countB= 0;
+	for(c=0;c<brickColumnCount;c++){
+		for(r=0;r<brickRowCount;r++){
+			var b = bricks[c][r];			
+			if(b.status==1){
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+					console.log("I've been hit");
+					dy=-dy;
+					b.status=0;
+					
+				}
+			}
+		}
+	}
+}
 
 //instantiate functions (classes)
 var ball = new Ball();
 var paddle = new Paddle();//called in draw method
-
+var bricks = new Bricks();//called in draw method
 //call methods
 function initialize(){
+	bricks.generateBricks();
+
+	
 	document.addEventListener("keydown", paddle.keyDownHandler, false);
 	document.addEventListener("keyup", paddle.keyUpHandler, false);	
+	
 	ball.refresh();
+
+	bricks.drawBricks();
+	
 }
 initialize();

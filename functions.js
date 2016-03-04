a81f49da721d35b6141b1e5bb4fdf2f3bfbe8cfc
Michael Quattrochi <mquattrochi@gmail.com>
2016-03-04 02:42:35 -0600

    Added lives, score, fixed errors, and finalized code
    Fixed error where ball would hit a brick, but the brick would not
    disappear.
    
    Fixed an error where ball would not center after hitting the ground.

--- functions.js
+++ functions.js
@@ -1,95 +1,68 @@
 /**
 *Ball class
 */
-//initialize Ball variables
 var canvas = document.getElementById("myCanvas");
 var ctx=canvas.getContext("2d");
 var x=canvas.width/2;//x ball position
-var y = canvas.height-30;//y ball position
+var y = canvas.height-30;
 var dx=2;//shift on x-axis
 var dy=-2;//shift on y-axis
 var ballRadius = 10;
 var rightPressed = false;
 var leftPressed=false;
-var score=0;
-$('#winner').hide();
+
 function Ball(){
 
-	
 }
-//draws gameball
 Ball.prototype.drawBall=function(){
 	ctx.beginPath();
 	ctx.arc(x,y,ballRadius,0,Math.PI*2);
-	//this.checkWalls();
 	fillStyle="#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
-Ball.prototype.drawPaddle=function(){
+Ball.prototype.checkWalls=function(){
+	var hitLeftSide = x+dx>canvas.width-ballRadius;
+	var hitRightSide = x+dx<ballRadius;
+	var hitTop = y+dy<ballRadius;
+	var hitPaddle = y+dy>canvas.height-ballRadius;
+	var hitBottom = x>paddleX && x <paddleX + paddleWidth;
 
-}
-//dynamically draw balls position
-Ball.prototype.draw=function(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	paddle.drawPaddle();//must be called in draw
-	paddle.movePaddle();//must be called in draw	
-	this.drawBall();	
-	s.drawScore();
-	bricks.collisionDetection();
-	x+=dx;
-	y+=dy;
-	this.x=x;
-	this.y=y;
-	this.checkWalls(this.x,this.y);
-}
-
-//refresh frame every 10milliseconds to see location of ball
-Ball.prototype.refresh=function(){
-	
-	setInterval(this.draw.bind(this),15);
-
-}
-
-
-
-Ball.prototype.checkWalls=function(x,y){
-	//reverse direction if ball hits left or right
-	if(x+dx>canvas.width-ballRadius || x+dx<ballRadius){
+	if(hitLeftSide||hitRightSide){
 		dx=-dx;
-	}
+	}	
+	if(hitTop){
+		dy=-dy;	
 
-	//if ball hits top then reverse, if ball hits bottom then show modal message
-	if(y + dy <ballRadius){
-		dy=-dy;
-
-	//if ball hits paddle, then reverse			
-	}else if(y+dy>canvas.height-ballRadius){
-
-		if(x>paddleX && x <paddleX + paddleWidth){
+	}else if(hitPaddle){
+		if(hitBottom){
 			dy=-dy;
-		}else{
-			$('#myModal').modal("show");
-			$('#closeModal').onclick=function(){
 
-				document.location.reload();
-				$('#myModal').modal('hide');
+		}else{
+			lives--;
+			if(!lives){
+				dx=0;
+				dy=0;
+				$('#myModal').modal("show");
+				$('#closeModal').onclick=function(){				
+					$('#myModal').modal('hide');
+				}
+				//do some function here
+			}else{				
+				x=canvas.width/2;
+				y=canvas.height-30;
+				dx=2;
+				dy=-2;
+				paddleX=(canvas.width-paddleWidth)/2;
+				
 			}			
 		}
-	}
-	
+	}	
 }
-	
-	
-
-
 
 /**
 *Paddle class
 */
-
-//initialize Paddle variables
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
@@ -97,7 +70,6 @@ var paddleX = (canvas.width-paddleWidth)/2;
 function Paddle(){
 
 }
-
 Paddle.prototype.drawPaddle=function(){
 	ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -105,7 +77,6 @@ Paddle.prototype.drawPaddle=function(){
     ctx.fill();
     ctx.closePath();
 }
-
 Paddle.prototype.keyDownHandler=function(e){
 	if(e.keyCode==39){
 		rightPressed=true;
@@ -113,7 +84,6 @@ Paddle.prototype.keyDownHandler=function(e){
 		leftPressed=true;
 	}
 }
-
 Paddle.prototype.keyUpHandler=function(e){
 	if(e.keyCode==39){
 		rightPressed=false;
@@ -121,17 +91,14 @@ Paddle.prototype.keyUpHandler=function(e){
 		leftPressed=false;
 	}	
 }
-
-/**
-*
-*/
 Paddle.prototype.mouseMoveHandler=function(e){
 	var relativeX=e.clientX - canvas.offsetLeft;
+	//check if paddle has moved out of view. If it has, change the relative position
+	//of the paddle to half of the paddle width
 	if(relativeX > 0 && relativeX < canvas.width){
 		paddleX=relativeX-paddleWidth/2;
 	}
 }
-
 Paddle.prototype.movePaddle=function(){
 	if(rightPressed && paddleX < canvas.width-paddleWidth){
 		paddleX+=7;
@@ -142,12 +109,9 @@ Paddle.prototype.movePaddle=function(){
 	y=y+dy;
 }
 
-
 /**
 *Brick class
 */
-
-//Brink variables
 var brickRowCount=3;
 var brickColumnCount=5;
 var brickWidth=75;
@@ -160,25 +124,19 @@ var bricks = [];
 function Bricks(){
 	
 }
-
-//generates bricks in a two-dimensional array
 Bricks.prototype.generateBricks=function(){
 	for(col=0;col<brickColumnCount;col++){
 		bricks[col]=[];
 		for(row=0;row<brickRowCount;row++){
-			bricks[col][row]={x:0,y:0,status:1};
+			bricks[col][row]={x:0,y:0,hitStatus:1};
 
 		}
 	}
 }
-
-//draws bricks to canvas
-Bricks.prototype.drawBricks=function(){	
-	//this.collisionDetection();
+Bricks.prototype.drawBricks=function(){
 	for(col=0;col<brickColumnCount;col++){
 		for(row=0;row<brickRowCount;row++){
-			//checks if bricks status is 1. If 1, draw brick. If 0, remove brick.
-			if(bricks[col][row].status==1){
+			if(bricks[col][row].hitStatus==1){
 				var brickX = (col*(brickWidth+brickPadding))+brickOffsetLeft;
 				var brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
 				bricks[col][row].x=brickX;
@@ -193,32 +151,21 @@ Bricks.prototype.drawBricks=function(){
 		}
 	}
 }
-
-/**
-*Check if ball hits a brick
-*
-*x-position of the ball is > x-position of brick
-*x-position of the ball is <x-position of brick+width
-*y-position of the ball is > y-position of the brick
-*y-position of ball is < y-positoin of the brick plus its height
-*/
 Bricks.prototype.collisionDetection=function(){
 	bricks.drawBricks();
 	var countB= 0;
-	for(c=0;c<brickColumnCount;c++){
-		for(r=0;r<brickRowCount;r++){
-			var b = bricks[c][r];			
-			if(b.status==1){
-				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
-					console.log("I've been hit");
+	for(col=0;col<brickColumnCount;col++){
+		for(row=0;row<brickRowCount;row++){
+			var brick = bricks[col][row];			
+			if(brick.hitStatus==1){
+				if(x > brick.x && x < brick.x+brickWidth && y > brick.y && y < brick.y+brickHeight){
 					dy=-dy;
-					b.status=0;
-					score++;//add points each time block is hit
-
-					//check if winner
-					if(score==brickRowCount*brickColumnCount){
+					brick.hitStatus=0;
+					score++;				
+					if(player.isWinner()){
 						$('#winner').show();
-						//document.location.reload();
+						dx=0;
+						dy=0;
 					}
 					
 				}
@@ -227,43 +174,58 @@ Bricks.prototype.collisionDetection=function(){
 	}
 }
 
-
-
 /**
-*Score class
+*Player class
 */
-
-
-function Score(){
+var score = 0;
+var lives = 3;
+function Player(){
 
 }
-Score.prototype.drawScore=function(){
+Player.prototype.drawScore=function(){
 	ctx.font="16px Arial";
 	ctx.fillStyle="#0095DD";
 	ctx.fillText("Score: "+ score,8,20);
 }
+Player.prototype.drawLives=function(){
+	ctx.font="16px Arial";
+	ctx.fillStyle="#0095DD";
+	ctx.fillText("Lives: "+lives,canvas.width-65,20);
+}
+Player.prototype.isWinner=function(){
+	if(score==brickRowCount*brickColumnCount){
+		return true;
+	}
+	return false;
+}
 
 
-//instantiate functions (classes)
 var ball = new Ball();
-var paddle = new Paddle();//called in draw method
-var bricks = new Bricks();//called in draw method
-var s = new Score();
-//call methods
-function initialize(){
-	bricks.generateBricks();
-	
+var paddle = new Paddle();
+var bricks = new Bricks();
+var player = new Player();
 
-	
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	paddle.drawPaddle();//must be called in draw
+	paddle.movePaddle();//must be called in draw	
+	ball.drawBall();	
+	player.drawScore();
+	player.drawLives();
+	bricks.collisionDetection();
+	x+=dx;
+	y+=dy;
+	ball.checkWalls();
+	requestAnimationFrame(draw);
+}
+
+function initialize(){
+	$('#winner').hide();
 	document.addEventListener("keydown", paddle.keyDownHandler, false);
 	document.addEventListener("keyup", paddle.keyUpHandler, false);
 	document.addEventListener("mousemove",paddle.mouseMoveHandler,false);
-	s.drawScore();
-	ball.refresh();
-
-	bricks.drawBricks();
-
-
-	
+	bricks.generateBricks();	
+	draw();
 }
+
 initialize();
